a0a8c62d94767d83644530955a5af8b99aefd2c3
Yizhe Wang <yizhe87@gmail.com>
2020-11-21 16:40:09 -0800

    delete mongo

--- learnmongodb/models/mariochar.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-
-const MarioCharSchema = new Schema({
-  name: String,
-  weight: Number,
-});
-
-export default MarioChar = mongoose.model('mariochar', MarioCharSchema);
--- learnmongodb/test/demo_test.js
+++
@@ -1,9 +0,0 @@
-const assert = require('assert');
-const mocha = require('mocha');
-//describe a test
-describe('saving to the database', function () {
-  //
-  it('adds two numbers together', function () {
-    assert(1 + 2 === 3);
-  });
-});
